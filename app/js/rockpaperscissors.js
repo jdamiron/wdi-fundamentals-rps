4ad0d3f66e012514b2c7ea0f82a5cb772e34e9d9
Jeremiah Damiron <jeremiahdamiron@gmail.com>
2016-01-04 14:16:32 -0500

    Update on player and computer moves

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -25,24 +25,46 @@ function getPlayerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return var move || getInput();
-    }
+    return move || getInput();
+}
 
 function getComputerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return var move || randomPlay();
-    }
+    return move || randomPlay();
+}
 
 function getWinner(playerMove,computerMove) {
     var winner;
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    /* YOUR CODE HERE */
-    return winner;
+    if 
+    ((playerMove === 'rock' && computerMove === 'scissors')||
+    (playerMove === 'scissors' && computerMove === 'paper')||
+    (playerMove === 'paper' && computerMove === 'rock'))
+    
+    {
+        winner = 'player';
 }
+    
+else if 
+    ((playerMove === 'scissors' && computerMove === 'rock')||
+    (playerMove === 'paper' && computerMove === 'scissors')||
+    (playerMove === 'rock' && computerMove === 'paper'))
+    
+    {
+        winner = 'computer';
+}
+
+else if
+    (playerMove === computerMove)
+    {
+        winner = 'tie';
+}
+}
+
 
 function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
